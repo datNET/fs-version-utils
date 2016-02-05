1f4b72ce5bba8b70851e064e3a76c9327d609b8a
Mathew Glodack <glodack@gmail.com>
2016-02-04 21:42:25 -0800

    Create test solution
    * Add simple assertion test
    * Add nuget packages: NUnit, FsUnit, NUnitTestAdapter

---
+++ tests/FSharpVersionUtils.Tests/AssemblyInfo.fs
@@ -0,0 +1,42 @@
+﻿namespace FSharpVersionUtils.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharpVersionUtils.Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FSharpVersionUtils.Tests")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("b778db0c-1657-41b3-af14-af25b63ed92a")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
+[<assembly: AssemblyFileVersion("0.0.1.0")>]
+[<assembly: AssemblyInformationalVersion("0.0.1")>]
+
+do
+    ()
---
+++ tests/FSharpVersionUtils.Tests/versionTests.fs
@@ -0,0 +1,8 @@
+﻿module Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Assert that true is equal to True`` () =
+    true |> should be True
