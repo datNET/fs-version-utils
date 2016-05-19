c092a748c30dacb9ef47cdf0cb67a13c49928750
Andrew William Seward <awswrd@gmail.com>
2016-05-18 23:17:57 -0700

    Start adding tests

---
+++ tests/FSharpVersionUtils.Tests/semVerTests.fs
@@ -0,0 +1,28 @@
+﻿module SemVerTests
+
+open FsUnit
+open NUnit.Framework
+open datNET
+
+//[<Test>]
+let ``Should parse '1'`` () =
+  let (expected: SemVer.SemVer) =
+    {
+      Major = 1
+      Minor = 0
+      Patch = 0
+      Pre   = None
+      Meta  = None
+    }
+  SemVer.parse "1" |> should equal expected
+
+let ``Should parse '1.'`` () =
+  let (expected: SemVer.SemVer) =
+    {
+      Major = 1
+      Minor = 0
+      Patch = 0
+      Pre   = None
+      Meta  = None
+    }
+  SemVer.parse "1." |> should equal expected
--- tests/FSharpVersionUtils.Tests/versionTests.fs
+++ tests/FSharpVersionUtils.Tests/versionTests.fs
@@ -2,6 +2,7 @@
 
 open NUnit.Framework
 open FsUnit
+open datNET
 open datNET.Version
 
 let private _version = new System.Version(1, 1, 1)
@@ -9,3 +10,16 @@ let private _version = new System.Version(1, 1, 1)
 [<Test>]
 let ``Should build correct semantic version using 1.1.1`` () =
     BuildSemVer 1 1 1 |> should equal _version
+
+[<Test>]
+let ``Should parse '1'`` () =
+  let (expected: SemVer.SemVer) =
+    {
+      Major = 1
+      Minor = 0
+      Patch = 0
+      Pre   = None
+      Meta  = None
+    }
+  SemVer.parse "1" |> should equal expected
+
