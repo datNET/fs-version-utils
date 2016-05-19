3fe4228cd398f6c6b4b95a1c3e60447c7a3635de
Andrew William Seward <awswrd@gmail.com>
2016-05-18 23:33:30 -0700

    Slightly more tests

--- tests/FSharpVersionUtils.Tests/semVerTests.fs
+++ tests/FSharpVersionUtils.Tests/semVerTests.fs
@@ -1,28 +1,44 @@
-﻿module SemVerTests
+﻿namespace datNET
 
-open FsUnit
-open NUnit.Framework
-open datNET
+module SemVerTests =
 
-//[<Test>]
-let ``Should parse '1'`` () =
-  let (expected: SemVer.SemVer) =
-    {
-      Major = 1
-      Minor = 0
-      Patch = 0
-      Pre   = None
-      Meta  = None
-    }
-  SemVer.parse "1" |> should equal expected
+  open FsUnit
+  open datNET
+  open NUnit.Framework
 
-let ``Should parse '1.'`` () =
-  let (expected: SemVer.SemVer) =
-    {
-      Major = 1
-      Minor = 0
-      Patch = 0
-      Pre   = None
-      Meta  = None
-    }
-  SemVer.parse "1." |> should equal expected
+  [<Test>]
+  let ``Parse 1`` () =
+    let (expected: SemVer.SemVer) =
+      {
+        Major = 1
+        Minor = 0
+        Patch = 0
+        Pre   = None
+        Meta  = None
+      }
+    SemVer.parse "1" |> should equal expected
+
+  [<Test>]
+  let ``parse 1.`` () =
+    let (expected: SemVer.SemVer) =
+      {
+        Major = 1
+        Minor = 0
+        Patch = 0
+        Pre   = None
+        Meta  = None
+      }
+    SemVer.parse "1." |> should equal expected
+
+  [<Test>]
+  let ``Parse 1.1`` () =
+    let (expected: SemVer.SemVer) =
+      {
+        Major = 1
+        Minor = 1
+        Patch = 0
+        Pre   = None
+        Meta  = None
+      }
+
+    SemVer.parse "1.1" |> should equal expected
